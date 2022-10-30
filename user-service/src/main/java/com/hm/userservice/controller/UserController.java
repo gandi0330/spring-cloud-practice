2df5b9f082420696ed32fb3ee2668d7cd02a60d5
gandi0330 <gandi0330@naver.com>
2022-10-30 17:31:56 +0900

    user-service init

---
+++ user-service/src/main/java/com/hm/userservice/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.hm.userservice.controller;
+
+import com.hm.userservice.dto.UserDto;
+import com.hm.userservice.service.UserService;
+import com.hm.userservice.vo.Greeting;
+import com.hm.userservice.vo.RequestUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+    private Environment env;
+    @Autowired
+    private Greeting greeting;
+    private UserService userService;
+
+    @Autowired
+    public UserController(Environment env, UserService userService) {
+        this.env = env;
+        this.userService = userService;
+    }
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        return "Create user method is called";
+    }
+}
---
+++ user-service/src/main/java/com/hm/userservice/service/UserService.java
@@ -0,0 +1,7 @@
+package com.hm.userservice.service;
+
+import com.hm.userservice.dto.UserDto;
+
+public interface UserService {
+    UserDto createUser(UserDto userDto);
+}
